c97e38e1f8ba9d1b07654b808704a8ff336a2592
Alexisca <150233709+Thealexisca22@users.noreply.github.com>
2025-06-23 18:48:58 +0000

    antes de correcion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let term = [".com", ".net", ".us", ".io", "etc."]
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < term.length; c++) {
+          let final = pronoun[i] + adj[a] + noun[b] + term[c];
+          console.log(final);
+
+        }
+
+
+      }
+
+    }
+
+  }
 };
